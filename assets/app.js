11b2006ff42fc8500fb0398d6a2591e36252f5f9
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-01-14 12:17:23 -0500

    updated CSS and JS

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    //test flag
+    const test = false;
+
+    //get the times from the moment
+    const timeNow = moment().format('LLLL');
+    console.log(timeNow)
+
+});
