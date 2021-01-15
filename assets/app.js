91d44ae222c19d2a019e56b3462773833efe58cd
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-01-14 21:10:18 -0500

    updated JS

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,46 @@ $(document).ready(function() {
     //test flag
     const test = false;
 
-    //get the times from the moment
+    //show the date and time from the present moment
     const timeNow = moment().format('LLLL');
     console.log(timeNow)
+
+    let $headingDate = $("#currentDay");
+    $headingDate.text(timeNow);
+
+    let nowHour24 = moment().format('H');
+    console.log(nowHour24)
+    let nowHour12 = moment().format('h');
+    console.log(nowHour12)
+
+    if (test) {
+        nowHour24 = 13;
+        nowHour12 = 1;
+    }
+
+    //using a save icon to save the event in the daily calendar
+    const saveIcon = $("#display-icon")
+
+    // Get the stored items from the to-do list from local storage
+
+    //If the plans were retrieved from local storage, update the plan array to it
+
+    //set variable referencing planner element
+    let $plannerDiv = $("#formPlanner");
+    $plannerDiv.empty();
+   
+
+    // build the calendar by row for a fixed number of hours
+    for (let hour =9; hour <=17; hour ++) {
+        let index = hour -9;
+    }
+
+    // build the row components in the calendar element
+
+    let $rowDiv =$("<div>");
+    $rowDiv.addClass("row");
+    $rowDiv.addClass("plannerRow");
+    $rowDiv.attr("hour-index", hour);
+
 
 });
